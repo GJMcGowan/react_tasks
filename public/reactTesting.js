f5b07f045d14e5ebec4edb2d0f378248b3d9ba69
George McGowan <geomcgowan@deloitte.co.uk>
2015-11-09 17:48:50 +0000

    Changing file location of JS to clear things up

--- public/reactTesting.js
+++ public/reactTesting.js
@@ -1,19 +1,59 @@
 var tasks = [
 	{text: "Water Plants", complete:"false",  due_date: "01/01/2016"}
-]
+];
 
-// ES5 version
-var taskTable = React.createClass({
+var TaskTable = React.createClass({
 	render: function() {
 		return (
-			<div className="taskTable">
-				Insert task table
-			</div>
+			<table className="taskTable">
+				<thead>
+					<tr>
+						<th>
+							Task
+						</th>
+						<th>
+							Complete?
+						</th>
+						<th>
+							Due
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					<TaskList />
+				</tbody>
+			</table>
+		);
+	}
+});
+
+var TaskList = React.createClass({
+	render: function() {
+		return (
+			<Task text="Feed Cat"></Task>
+		);
+	}
+});
+
+var Task = React.createClass({
+	render: function() {
+		return (
+			<tr className="task">
+				<td>
+					{this.props.text}
+				</td>
+				<td>
+					{this.props.complete}
+				</td>
+				<td>
+					Blank
+				</td>
+			</tr>
 		);
 	}
 });
 
 ReactDOM.render(
-	<taskTable />,
+	<TaskTable />,
 	document.getElementById('content')
 );
